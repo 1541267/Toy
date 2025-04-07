438085d68d6c58b59da7891f22df1ca4bdd8231b
1541267 <1541267@naver.com>
2025-04-07 17:31:41 +0900

    추가

---
+++ src/main/java/Algorithm/BigNumberCalc.java
@@ -0,0 +1,34 @@
+package Algorithm;
+
+public class BigNumberCalc {
+	public static void main(String[] args) {
+
+		String a = "18446744073709551615";
+		String b = "287346502836570928366";
+		// String a = "123";
+		// String b = "456";
+
+		// BigInteger c = new BigInteger(a);
+		// BigInteger d = new BigInteger(b);
+		System.out.println(sum(a, b));
+	}
+
+
+
+	private static String sum(String a, String b) {
+		StringBuilder sb = new StringBuilder();
+		int i = a.length() - 1;
+		int j = b.length() - 1;
+		int overValue = 0;
+		while (i >= 0 || j >= 0 || overValue > 0) {
+			int digitA = (i >= 0) ? a.charAt(i--) - '0' : 0;
+			int digitB = (j >= 0) ? b.charAt(j--) - '0' : 0;
+
+			int sum = digitA + digitB + overValue;
+			sb.append(sum % 10);
+			overValue = sum / 10;
+		}
+		return sb.reverse().toString();
+	}
+}
+
---
+++ src/main/java/Algorithm/DigitNumberCheck.java
@@ -0,0 +1,21 @@
+package Algorithm;
+
+import java.io.IOException;
+
+public class DigitNumberCheck {
+	public static void main(String[] args) throws IOException {
+
+	}
+
+	private static boolean check(int n) {
+
+		while (n > 0) {
+			if (n % 10 != 0 && n % 10 != 5) {
+				return false;
+			}
+			n /= 10;
+		}
+
+		return true;
+	}
+}
